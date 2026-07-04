e6711ccc7904231fef597b0b4fed4bb286c58444
manith003 <manithkumar8190@gmail.com>
2026-07-04 15:41:06 +0530

    day01 done

---
+++ day01/script.js
@@ -0,0 +1,15 @@
+// let a = 10;
+// let b = 20;
+
+
+// // console.log(a, b)
+
+// [a, b] = [b, a];
+
+// console.log(a, b);
+
+// module
+// console.log(Math.floor(10/3))
+let a = 10
+
+console.log(a++ + ++a)
