8c0361454540189c55ff95e59c1f8ed4d777df14
manith003 <manithkumar8190@gmail.com>
2026-07-09 23:01:08 +0530

    day04 ongoing.

---
+++ day04/script.js
@@ -0,0 +1,23 @@
+// let n = Number(prompt("Enter a Number: "));
+
+// function printNumber(n) {
+//   if (isNaN(n)) {
+//     console.log("invalid Input.");
+//     return;
+//   } 
+
+//   let i;
+//   for (i = 1; i <= n; i++) {
+//     console.log("hello World");
+//   }
+//   console.log("fail at " + i);
+// }
+
+// printNumber(n);
+
+
+let n = 5
+
+for(let i = n; i>=1; i--){
+    console.log(i)
+}
