a2394957206af2bf0aae5a5536357f7e4d49627b
manith003 <manithkumar8190@gmail.com>
2026-07-09 23:27:27 +0530

    day4 onging part2

--- day04/script.js
+++ day04/script.js
@@ -16,8 +16,23 @@
 // printNumber(n);
 
 
-let n = 5
+// let n = 5
 
-for(let i = n; i>=1; i--){
-    console.log(i)
-}
+// for(let i = n; i>=1; i--){
+//     console.log(i)
+// }
+
+
+// for(let i=1;i<=10;i++){
+// console.log(`5 * ${i} = ${5*i}`)
+// }
+
+
+// let n = 10
+// let sum = 0 
+
+// for(let i =1; i<=n ; i++){
+//     sum+=i
+// }
+
+// console.log(sum)
