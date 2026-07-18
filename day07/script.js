c24efdd1f8014d861def5ceff7118ab83a57fe4e
manith003 <manithkumar8190@gmail.com>
2026-07-18 23:55:13 +0530

    day07 nested programming

---
+++ day07/script.js
@@ -0,0 +1,27 @@
+// for (let i = 1; i <= 1; i++) {
+//   process.stdout.write(" * ");
+// }
+// console.log();
+// for (let i = 1; i <= 2; i++) {
+//   process.stdout.write(" * ");
+// }
+// console.log();
+// for (let i = 1; i <= 3; i++) {
+//   process.stdout.write(" * ");
+// }
+// console.log();
+// for (let i = 1; i <= 4; i++) {
+//   process.stdout.write(" * ");
+// }
+// console.log();
+// for (let i = 1; i <= 5; i++) {
+//   process.stdout.write(" * ");
+// }
+// console.log();
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write(" * ");
+  }
+  console.log();
+}
